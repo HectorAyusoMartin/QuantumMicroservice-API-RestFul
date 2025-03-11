866109b68486d77dbb43ab1d8a7a2ec17fad8c0f
HectorAyusoMartin <hector.ayuso.martin@gmail.com>
2025-03-11 17:38:21 +0100

    Añadida la función de QRNG.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from app.config import MONGO_URI , JWT_SECRET
-from app.routers import auth, users, quantum_random
+from app.routers import auth, users, quantum_random, keys
 
 app = FastAPI(title="Microservicio de generación de aleatoriedad cuántica")
 
 app.include_router(auth.router,prefix="/auth",tags=["Autenticación"])
 app.include_router(users.router,prefix="/users",tags=["Usuarios"])
-app.include_router(quantum_random.router,prefix="/random",tags=["Aleatoriedad cuántica"])
+app.include_router(quantum_random.router,prefix="/random",tags=["Aleatoriedad cuántica(QRNG)"])
+app.include_router(keys.router,prefix="/keys",tags=["Generador de claves seguras(Quantum-randomness)"])
 
                    
                    
